063d3bf64b6c7bcf974a5735fe409d31e8368673
Nastya Kot <VanderOfCat@gmail.com>
2026-01-08 17:40:05 +0300

    tooltip rendering

--- src/main/java/melonslise/locks/events/LocksRenderEvents.java
+++ src/main/java/melonslise/locks/events/LocksRenderEvents.java
@@ -1,7 +1,14 @@
 package melonslise.locks.events;
 
+import melonslise.locks.Locks;
 import melonslise.locks.common.init.LocksComponents;
+import melonslise.locks.common.init.LocksItemTags;
+import melonslise.locks.common.init.LocksItems;
 import melonslise.locks.graphics.LocksRendering;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class LocksRenderEvents {
     public static void register() {
@@ -12,11 +19,35 @@ public class LocksRenderEvents {
             if (!lock.isEmpty())
                 LocksRendering.renderLock(locked, dir, light, poseStack, multiBufferSource);
         });
-//        HudRenderCallback.EVENT.register((guiGraphics, v) -> {
-//            var mc = Minecraft.getInstance();
-//            var player = mc.player;
-//            player.look
-//            guiGraphics.renderTooltip(mc.font, );
-//        });
+        HudRenderCallback.EVENT.register((guiGraphics, v) -> {
+            var mc = Minecraft.getInstance();
+            var level = mc.level;
+            var player = mc.player;
+            var mainitem = player.getMainHandItem();
+            if (!(
+                mainitem.is(LocksItemTags.KEYS) ||
+                mainitem.is(LocksItemTags.LOCK_PICKS)
+            ))
+                return;
+            var hit = mc.hitResult;
+            if (hit == null)
+                return;
+            if (hit.getType() != HitResult.Type.BLOCK)
+                return;
+            var blockhit = (BlockHitResult)hit;
+            var blockpos = blockhit.getBlockPos();
+            var blockentity = level.getBlockEntity(blockpos);
+            if (blockentity == null)
+                return;
+            var locked = blockentity.getComponent(LocksComponents.LOCKED);
+            var item = locked.getLock();
+            if (item.isEmpty())
+                return;
+            var w = guiGraphics.guiWidth();
+            var h = guiGraphics.guiHeight();
+            var x = w/2;
+            var y = h/2;
+            guiGraphics.renderTooltip(mc.font, item, x, y);
+        });
     }
 }
