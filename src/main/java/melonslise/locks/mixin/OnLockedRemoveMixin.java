91775cc2a39fe8fbb2b2da5a6ff3e5e2c949131f
Nastya Kot <VanderOfCat@gmail.com>
2026-01-08 17:01:30 +0300

    drop locks on break

---
+++ src/main/java/melonslise/locks/mixin/OnLockedRemoveMixin.java
@@ -0,0 +1,27 @@
+package melonslise.locks.mixin;
+
+import melonslise.locks.common.init.LocksComponents;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockBehaviour.class)
+public class OnLockedRemoveMixin {
+    @Inject(at = @At(value = "HEAD"), method = "onRemove(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)V")
+    private void dropLock(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2, boolean bl, CallbackInfo ci) {
+        if (blockState.hasBlockEntity() && !blockState.is(blockState2.getBlock())) {
+            var be = level.getBlockEntity(blockPos);
+            var locked = be.getComponent(LocksComponents.LOCKED);
+            var item = locked.getLock();
+            if (item.isEmpty())
+                return;
+            Containers.dropItemStack(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), item);
+        }
+    }
+}
